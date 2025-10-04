a6cef87ac76c487c1ebf62774c5303f75f2af9bf
蓮見昌浩 <mhasumi@hasumimasahironoMacBook-Air.local>
2025-10-05 02:27:12 +0900

    update

--- py_program.py
+++ py_program.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_quadratic(A, B, C, D, E):
+def plot_quadratic(A, B, C, N, M):
     func = "y = "
     conp_square = func
     if A == 1:
@@ -54,37 +54,37 @@ def plot_quadratic(A, B, C, D, E):
     plt.clf()
     # x の範囲を決める（-10〜10を100点）
     # x = np.linspace(-10, 10, 400)
-    x = np.linspace(D, E, 400)
+    x = np.linspace(N, M, 400)
     y = A * x**2 + B * x + C
 
     axle = -B / (2 * A)
-    if D < axle < E and A > 0:
-        x_max = E if axle - D < E - axle else D
+    if N < axle < M and A > 0:
+        x_max = M if axle - N < M - axle else N
         x_min = axle
         y_max = A * x_max**2 + B * x_max + C
         y_min = A * x_min**2 + B * x_min + C
-    elif D < axle < E and A < 0:
-        x_min = E if axle - D < E - axle else D
+    elif N < axle < M and A < 0:
+        x_min = M if axle - N < M - axle else N
         x_max = axle
         y_min = A * x_min**2 + B * x_min + C
         y_max = A * x_max**2 + B * x_max + C
-    elif axle <= D:
-        x_max = E if A > 0 else D
-        x_min = D if A > 0 else E
+    elif axle <= N:
+        x_max = M if A > 0 else N
+        x_min = N if A > 0 else M
         y_max = A * x_max**2 + B * x_max + C
         y_min = A * x_min**2 + B * x_min + C
-    elif axle >= E:
-        x_max = D if A > 0 else E
-        x_min = E if A > 0 else D
+    elif axle >= M:
+        x_max = N if A > 0 else M
+        x_min = M if A > 0 else N
         y_max = A * x_max**2 + B * x_max + C
         y_min = A * x_min**2 + B * x_min + C
 
     print(f"最大値　　 : ({x_max:.1f}, {y_max:.1f})")
     print(f"最小値　　 : ({x_min:.1f}, {y_min:.1f})")
 
-    x_range = E - D
+    x_range = M - N
     y_range = y_max - y_min
-    x_mid = (D + E) / 2
+    x_mid = (N + M) / 2
     y_mid = (y_max + y_min) / 2
     
     # グラフ描画
@@ -97,8 +97,8 @@ def plot_quadratic(A, B, C, D, E):
     plt.axhline(0, color="black", linewidth=0.8)  # x軸
     plt.axvline(0, color="black", linewidth=0.8)  # y軸
     
-    plt.axvline(D, color="gray", linestyle="--")
-    plt.axvline(E, color="gray", linestyle="--")
+    plt.axvline(N, color="gray", linestyle="--")
+    plt.axvline(M, color="gray", linestyle="--")
     
     plt.scatter([x_max], [y_max], color="red", zorder=5, label="Max")
     plt.scatter([x_min], [y_min], color="blue", zorder=5, label="Min")
@@ -112,4 +112,4 @@ def plot_quadratic(A, B, C, D, E):
 
 
 if __name__ == "__main__":
-    plot_quadratic(a, b, c, d, e)
+    plot_quadratic(a, b, c, n, m)
